68782bbe88597467d63b033d8e4810e3c5581631
sharath9823z <sharath9823@gmail.com>
2025-12-01 11:14:17 +0530

    Create Rectangle constructor overload.java

---
+++ Rectangle constructor overload.java
@@ -0,0 +1,27 @@
+class Rectangle {
+    int length, width;
+
+    Rectangle(int l, int w) {
+        length = l;
+        width = w;
+    }
+
+    Rectangle(int side) {
+        length = side;
+        width = side;
+    }
+
+    int area() {
+        return length * width;
+    }
+}
+
+public class RectangleConstructorOverload {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(10, 5);
+        Rectangle r2 = new Rectangle(7);
+
+        System.out.println("Area of Rectangle: " + r1.area());
+        System.out.println("Area of Square: " + r2.area());
+    }
+}
